7cadaa9c9c39ea583f99d8f4dd252a2234f75b60
tlenparty <tevez-32@inbox.ru>
2021-08-16 12:43:49 +0300

    5 homework commit 16.08.21

--- app/src/main/java/com/geekbrains/pictureoftheday/picture/bottom_navigation_drawer_fragment/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/geekbrains/pictureoftheday/picture/bottom_navigation_drawer_fragment/BottomNavigationDrawerFragment.kt
@@ -1,11 +1,14 @@
 package com.geekbrains.pictureoftheday.picture.bottom_navigation_drawer_fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.geekbrains.pictureoftheday.R
+import com.geekbrains.pictureoftheday.animations.AnimationsActivity
+import com.geekbrains.pictureoftheday.animations.AnimationsActivityBonus
 import com.geekbrains.pictureoftheday.databinding.BottomNavigationLayoutBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -26,9 +29,14 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
-                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                R.id.navigation_one -> activity?.let{
+                    startActivity(Intent(it, AnimationsActivity::class.java))
+                }
+                R.id.navigation_two -> activity?.let{
+                    startActivity((Intent(it,AnimationsActivityBonus::class.java)))
+                }
             }
+            dismiss()
             true
         }
     }
